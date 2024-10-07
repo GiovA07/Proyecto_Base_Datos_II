ce2f9bf850ea1cd5f8a5ca422d128e0a73d79352
Uriel-Gz <uriguzman150212@gmail.com>
2024-10-07 16:22:53 -0300

    mostrado de informe

--- app/src/main/java/proyecto_base_datos_ii/Utilities/InformPrinter.java
+++ app/src/main/java/proyecto_base_datos_ii/Utilities/InformPrinter.java
@@ -4,10 +4,35 @@ import java.io.*;
 
 public class InformPrinter {
 
+    private String init = "\n - - - Diferencias entre las Bases de datos - - - \n\n";
+    private String tableSection = "\n\n - - - Tablas que difieren - - -\n";
+    private String funcSection = "\n\n - - - Funciones que difieren - - -\n";
+    private String procSection = "\n\n Procedimientos almacenados que difieren\n";
+
     public void printFile() {
         try {
             FileWriter fw = new FileWriter("app/src/main/resources/Diferecnces_Inform.txt");
-            fw.write("...");
+            int a = tableSection.length();
+            int b = funcSection.length();
+            int c = procSection.length();
+
+            if (a == 35 && b == 38 && c == 43)
+                fw.write(init + " No se encontraron diferencias, bases de datos compatibles");
+            else if (a == 35 && b == 38 && c >= 43)
+                fw.write(init + procSection);
+            else if (a == 35 && b >= 38 && c == 43)
+                fw.write(init + funcSection);
+            else if (a == 35 && b >= 38 && c >= 43)
+                fw.write(init + funcSection + procSection);
+            
+            else if (a >= 35 && b == 38 && c == 43)
+                fw.write(init + tableSection);
+            else if (a >= 35 && b == 38 && c >= 43)
+                fw.write(init + tableSection + procSection);
+            else if (a >= 35 && b >= 38 && c == 43)
+                fw.write(init + tableSection + funcSection);
+            else if (a >= 35 && b >= 38 && c >= 43)
+                fw.write(init + tableSection + funcSection + procSection);
 
             fw.close();
         } catch (IOException e) {
