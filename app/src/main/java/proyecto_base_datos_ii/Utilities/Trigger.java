11f897d72a44a756f2408592d16472136b3f10d5
Lara Combina <lara.combina11@gmail.com>
2024-10-13 17:51:03 -0300

    comparation of triggers

--- app/src/main/java/proyecto_base_datos_ii/Utilities/Trigger.java
+++ app/src/main/java/proyecto_base_datos_ii/Utilities/Trigger.java
@@ -7,8 +7,8 @@ public class Trigger {
 
     public Trigger(String name, String action, String moment) {
         this.name = name;
-        this.action = action;
-        this.moment = moment;
+        this.action = action; //INSERT, UPDATE, DELETE 
+        this.moment = moment; //BEFORE, AFTER, INSTEADOF
     }
 
     public String getName() {
@@ -25,6 +25,74 @@ public class Trigger {
 
 
     public String toString() {
-        return "Trigger: " + name + " ejecucion: " + moment + " " + action + "\n";
+        return name + " " + moment + " " + action;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Trigger)) return false;
+        Trigger other = (Trigger) obj;
+        
+        if (!this.name.equals(other.name)) return false;
+        if (!this.action.equals(other.action)) return false;
+        if (!this.moment.equals(other.moment)) return false;
+        return true; 
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + action.hashCode();
+        result = 31 * result + moment.hashCode();
+        return result;
+    }
+
+    public String differencesToString(Trigger other) {
+        StringBuilder differences = new StringBuilder();
+        if(this.equals(other)){
+            differences.append("\nLos triggers son iguales: ");
+            differences.append(this.toString());
+            return differences.toString();
+        }
+    
+        differences.append("\nLos triggers difieren: ");
+        differences.append("\n 1: " + this);
+        differences.append("\n 2: "+ other);
+
+        if (!this.name.equals(other.name)){
+            differences.append("\n- Nombre: ");
+            differences.append(this.name);
+            differences.append(" vs. ");
+            differences.append(other.name);
+        }
+
+        if (!this.moment.equals(other.moment)){
+            differences.append("\n- Momento de Ejecucion: ");
+            differences.append(this.moment);
+            differences.append(" vs. ");
+            differences.append(other.moment);
+        }
+
+        if (!this.action.equals(other.action)){
+            differences.append("\n- Por acción: ");
+            differences.append(this.action);
+            differences.append(" vs. ");
+            differences.append(other.action);
+        }
+        return differences.toString();
+    }
+
+    public static void main(String[] args){
+        Trigger trigger1 = new Trigger("trigger_before_insert", "INSERT", "BEFORE");
+        Trigger trigger2 = new Trigger("trigger_after_insert", "INSERT", "AFTER");
+        Trigger trigger3 = new Trigger("trigger_before_update", "UPDATE", "BEFORE");
+        Trigger trigger4 = new Trigger("trigger_before_insert", "INSERT", "BEFORE"); // Same as trigger1
+        Trigger trigger5 = new Trigger("trigger_after_delete", "DELETE", "AFTER");
+
+        System.out.println(trigger1.differencesToString(trigger2));
+        System.out.println(trigger1.differencesToString(trigger3));
+        System.out.println(trigger1.differencesToString(trigger4));
+        System.out.println(trigger1.differencesToString(trigger5));
     }
 }
